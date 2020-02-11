89ae02f030c4d2220c013618712e5642cbeaf0c5
Zeth Kane <zkane@zipcodes-MacBook-Pro-9.local>
2020-02-11 18:27:18 -0500

    finished

--- src/main/java/io/zipcoder/StringsAndThings.java
+++ src/main/java/io/zipcoder/StringsAndThings.java
@@ -98,6 +98,17 @@ public class StringsAndThings {
      *            countTriple("a") // Should return 0
      */
     public Integer countTriple(String input){
-        return null;
+        int count = 0;
+        char c = '0';
+        for (int i = 0; i < input.length()-2; i++){
+            if (input.charAt(i) ==  input.charAt(i+1)  && input.charAt(i) == input.charAt(i+2)){
+                count++;
+            }
+
+
+
+        }
+
+        return count;
     }
 }
--- src/test/java/io/zipcoder/stringsandthings/GIsHappyTest.java
+++ src/test/java/io/zipcoder/stringsandthings/GIsHappyTest.java
@@ -33,7 +33,7 @@ public class GIsHappyTest {
     @Test
     public void gIsHappyTest3(){
         Boolean actual = stringsAndThings.gIsHappy("xxggyygxx");
-        Assert.assertTrue(actual);
+        Assert.assertFalse(actual);
     }
 
 }
